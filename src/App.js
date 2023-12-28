1f5a73ae0946751c78b57e86615db1d72e6897b0
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-12-28 17:58:07 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="main">
+        <div id="screen">
+          <div id="display">0</div>
+          <div id="result">0</div>
+        </div>
+        <table>
+          <tr>
+            <td colSpan="2">
+              <button id="clear" style={{backgroundColor: "rgb(150, 10, 10)"}}>AC</button>
+            </td>
+            <td>
+              <button id="divide">/</button>
+            </td>
+            <td>
+              <button id="multiply">x</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button id="seven">7</button>
+            </td>
+            <td>
+              <button id="eight">8</button>
+            </td>
+            <td>
+              <button id="nine">9</button>
+            </td>
+            <td>
+              <button id="subtract">-</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button id="four">4</button>
+            </td>
+            <td>
+              <button id="five">5</button>
+            </td>
+            <td>
+              <button id="six">6</button>
+            </td>
+            <td>
+              <button id="add">+</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button id="one">1</button>
+            </td>
+            <td>
+              <button id="two">2</button>
+            </td>
+            <td>
+              <button id="three">3</button>
+            </td>
+            <td rowSpan="2">
+              <button id="equals" style={{height: 135 + 'px'}}>=</button>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <button id="zero">0</button>
+            </td>
+            <td>
+              <button id="decimal">.</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
